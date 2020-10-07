19a9937d830285b2a9ad91c1c1e7b1c7050b99d3
Allen Xavier Arasan <xavierallem1999@gmail.com>
2020-10-07 21:03:07 +0200

    Replace iftt_demo.cxx

--- assignments/practice/problem1/iftt_demo.cxx
+++ assignments/practice/problem1/iftt_demo.cxx
@@ -20,7 +20,14 @@
 /* --- Standard Includes --- */
 #include <stdio.h>
 
+
 /* --- Project Includes --- */
+#include <shunyaInterfaces.h>
+#include <Wire.h>
+#include "bme280.h"
+#include <curl/curl.h>
+#include "iftt.h"
+#include <sys/resource.h>
 
 
 /*
@@ -30,35 +37,67 @@
  *  Solve all your problems here 
  *#####################################################################
  */
+using namespace rapidjson;
 
-/** 
- *  @brief Description on function_1
- *  
- *  Full description of the function
- *
- *  @return List all Function returns 
- */
 
-void function_1 (void)
-{
 
-        /*Make sure you comment every line */
-	
-}
-
-/** 
+/**
  *  @brief Description on main
- *  
- *  Full description of the function
  *
- *  @return List all Function returns 
+ *  Connects and gets temperature , creates json doc
+ * and publishes data to ifttt
+ *
+ *  @return 
  */
 
-int main (void)
-{
+int main(){
+	struct rusage r_usage;
+	getrusage(RUSAGE_SELF,&r_usage);
+	/* Variable to store auth key*/
+	chat auth[50];
+	printf("Enter TheIFTTT Auth key \n");
+	/* Take input of auth key */
+	scanf("%[^\n]%*c", auth);
+    /* Get termperature */
+    float temperature = getTemperature(); 
+	if(temperature>30)
+	{
+		/*Json file to be parsed*/
+		const char* json;
+		sprtinf(json."{\"Temperature\":30,\"Humidity\":%f}",temperature);
+		/*Initializing document variable*/
+		Document d;
+		/*Parsing the json */
+		d.Parse(json);
+		/*Initializing buffer*/
+		StringBuffer buffer;
+		/*Wrtier stream-*/
+		Writer<StringBuffer> writer(buffer);
+		/* Write the JSON document `d` into the buffer`*/
+		d.Accept(writer);
+		/* Publish the json values and auth */
+		POST_IFTTT(buffer.GetString(),auth);
+	}
+	if(r_usage.ru_idrss>1,000,000){
+		
+		const char* json1;
+		sprtinf(json1."{\"Ram Usage is \":%ld}",r_usage.ru_idrss);
+		/*Initializing document variable*/
+		Document d1;
+		/*Parsing the json */
+		d1.Parse(json1);
+		/*Initializing buffer*/
+		StringBuffer buffer1;
+		/*Wrtier stream-*/
+		Writer<StringBuffer> writer(buffer1);
+		/* Write the JSON document `d` into the buffer`*/
+		d1.Accept(writer);
+		/* Publish the json values and auth */
+		POST_IFTTT(buffer1.GetString(),auth);
 
-        /*Make sure you comment every line */
-        return 0;
-        
+
+	}
+	return 0;
 }
+
 
